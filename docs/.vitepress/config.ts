96c91a949e7cddd37805cf1b6b9fa11812f5c2e8
sutie <sususu_sutie@qq.com>
2023-04-27 14:29:19 +0800

    feat: update vitepress config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -77,7 +77,7 @@ export default defineConfig({
   // outDir: "../docs",
 
   lang: "zh-CN",
-  title: "VitePress-Note",
+  title: "Note",
   description: "VitePress 驱动的个人笔记.",
 
   lastUpdated: true,
@@ -117,12 +117,12 @@ export default defineConfig({
     sidebar,
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/susususutie/vitepress-note" },
+      { icon: "github", link: "https://github.com/susususutie/note" },
       {
         icon: {
           svg: '<svg width="24px" height="24px" viewBox="0 0 24 24" role="img" xmlns="http://www.w3.org/2000/svg"><path d="M11.984 0A12 12 0 0 0 0 12a12 12 0 0 0 12 12 12 12 0 0 0 12-12A12 12 0 0 0 12 0a12 12 0 0 0-.016 0zm6.09 5.333c.328 0 .593.266.592.593v1.482a.594.594 0 0 1-.593.592H9.777c-.982 0-1.778.796-1.778 1.778v5.63c0 .327.266.592.593.592h5.63c.982 0 1.778-.796 1.778-1.778v-.296a.593.593 0 0 0-.592-.593h-4.15a.592.592 0 0 1-.592-.592v-1.482a.593.593 0 0 1 .593-.592h6.815c.327 0 .593.265.593.592v3.408a4 4 0 0 1-4 4H5.926a.593.593 0 0 1-.593-.593V9.778a4.444 4.444 0 0 1 4.445-4.444h8.296z"/></svg>',
         },
-        link: "https://gitee.com/susususutie/vitepress-note",
+        link: "https://gitee.com/susususutie/note",
       },
     ],
     outlineTitle: "文档目录",
@@ -134,8 +134,8 @@ export default defineConfig({
     returnToTopLabel: "回到顶部",
     editLink: {
       pattern:
-        "https://github.com/susususutie/vitepress-note/edit/dev/src/:path",
-      text: "在 GitHub 上编辑(dev)",
+        "https://github.com/susususutie/note/edit/main/docs/:path",
+      text: "在 GitHub 上编辑",
     },
     footer: {
       message: "蛤? 👓",
